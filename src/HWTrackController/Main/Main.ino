2d42119d0fdd6331d6f70ea478a589f055ac7adf
Luke Karavolis <lukejrkaravolis@gmail.com>
2020-09-30 14:52:03 -0400

    Task and Scheduler

--- src/HWTrackController/Main/Main.ino
+++ src/HWTrackController/Main/Main.ino
@@ -6,7 +6,7 @@
 #include <stdint.h> // For standard types
 
 // C++ PROJECT INCLUDES
-// (None)
+#include "src/Scheduler.hpp" // For Scheduler
 
 static uint64_t currentTime;
 
@@ -15,24 +15,16 @@ void setup()
     Serial.begin(9600);
 
     pinMode(LED_BUILTIN, OUTPUT);
+
+    Scheduler::GetInstance().AddTask(new Task(toggleTask, 1000));
 }
 
 void loop()
 {
-    // Get the current time
-    currentTime = millis();
-
-    toggleTask();
+    Scheduler::GetInstance().RunTasks();
 }
 
-void toggleTask()
+void toggleTask(void* something)
 {
-    const uint32_t period = 1000;
-    static uint64_t lastUpdateTime = 0;
-
-    if (currentTime - lastUpdateTime >= period)
-    {
-        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-        lastUpdateTime = currentTime;
-    }
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 }
