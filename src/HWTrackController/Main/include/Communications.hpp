4a01742274b8a9456fcb8157faf7aaed49c40161
Luke Karavolis <lukejrkaravolis@gmail.com>
2020-10-05 13:37:53 -0400

    DetermineCode

--- src/HWTrackController/Main/include/Communications.hpp
+++ src/HWTrackController/Main/include/Communications.hpp
@@ -14,6 +14,17 @@ namespace Communications
 {
 
 /**
+ * @enum RequestCode
+*/
+enum class RequestCode
+{
+    INVALID = 1,
+
+    SET_SWITCH_POSITION = 96,
+    GET_SWITCH_POSITION = 97
+};
+
+/**
  * 
 */
 void CommsTask(void* something);
--- src/HWTrackController/Main/src/Communications.cpp
+++ src/HWTrackController/Main/src/Communications.cpp
@@ -11,14 +11,35 @@
 namespace Communications
 {
 
+/**
+ * 
+*/
+static RequestCode DetermineCode(String& rData)
+{
+    int spaceIndex = rData.indexOf(' ');
+    String codeString = rData.substring(0, spaceIndex);
+    int code = atoi(codeString.c_str());
+
+    switch (code)
+    {
+        case static_cast<int>(RequestCode::GET_SWITCH_POSITION):
+        case static_cast<int>(RequestCode::SET_SWITCH_POSITION):
+            return static_cast<RequestCode>(code);
+        default:
+            return RequestCode::INVALID;
+    }
+}
+
 void CommsTask(void* something)
 {
-    int receivedData = Serial.read();
-    if (receivedData != -1)
+    // Quickly return if nothing has been received
+    if (Serial.available() == 0)
     {
-        Serial.write("I received ");
-        Serial.println(receivedData, DEC);
+        return;
     }
+
+    String data = Serial.readStringUntil('\n');
+    RequestCode code = DetermineCode(data);
 }
 
 } // namespace Communications
