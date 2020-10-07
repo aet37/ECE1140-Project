3feeeee9f12a37a9ebe0aefe31d06f061752edf9
Collin Hough <chough724@gmail.com>
2020-10-07 19:34:27 -0400

    SWTrainController UI Updates

--- src/SWTrainController/UI/TrainController.py
+++ src/SWTrainController/UI/TrainController.py
@@ -1,29 +1,76 @@
 import os
-from PyQt5 import QtWidgets
-from PyQt5 import uic
+from PyQt5 import QtWidgets, uic
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QPushButton, QStackedWidget
 import sys
 
-class Ui(QtWidgets.QMainWindow):
+class SWTrainUi(QtWidgets.QMainWindow):
     def __init__(self):
-        super(Ui, self).__init__()
-        uic.loadUi('/Users/collinhough/Desktop/Classes/JuniorYear/Fall_2020_Classes/ECE1140/Code/UI/TrainController.ui', self)
+        super(SWTrainUi, self).__init__()
+        uic.loadUi('/Users/collinhough/Documents/Github/ECE1140-Project/src/SWTrainController/UI/TrainController.ui', self)
 
-        #self.submit_in = self.findChild(QtWidgets.QPushButton, 'submit_in') # Find the button
-        self.button = self.findChild(QtWidgets.QPushButton, 'TrainActions') # Find the button
-        self.button.clicked.connect(self.trainActions)
-        self.button = self.findChild(QtWidgets.QPushButton, 'Information') # Find the button
-        self.button.clicked.connect(self.information)
-        
-        # Create a toggle button
-        #self.button = QtWidgets.QPushButton("Toggle", self)
+        self.initUI()
+        self.stacked_widget.currentChanged.connect(self.set_button_state)
+        self.stacked_widget.setCurrentIndex(0)
 
+        # Define which pages buttons will take you to
+        self.button1.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(1))
+        self.button2.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(2))
+        self.return_button1.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.return_button2.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
 
-        self.button2 = self.findChild(QtWidgets.QPushButton, 'Logout') # Find the button
-        self.button2.clicked.connect(self.logout)
+        # Define logout button
+        self.logout_button = self.findChild(QtWidgets.QPushButton, 'Logout') # Find the button
+        self.logout_button.clicked.connect(self.logout)
+        self.logout_button2 = self.findChild(QtWidgets.QPushButton, 'Logout2') # Find the button
+        self.logout_button2.clicked.connect(self.logout)
+        self.logout_button3 = self.findChild(QtWidgets.QPushButton, 'Logout3') # Find the button
+        self.logout_button3.clicked.connect(self.logout)
 
         self.show()
+    
+    # Initialize buttons in UI
+    def initUI(self):
+        # Define buttons on main page
+        self.button1 = self.findChild(QtWidgets.QPushButton, 'TrainActions') # Find the button
+        self.button2 = self.findChild(QtWidgets.QPushButton, 'Information') # Find the button
+        self.stacked_widget = self.findChild(QtWidgets.QStackedWidget, 'stackedWidget') # Find stacked widget
+
+        # Define buttons on Train Actions Page
+        self.return_button1 = self.findChild(QtWidgets.QPushButton, 'MainMenu2')
+        self.doors_button = self.findChild(QtWidgets.QPushButton, 'Doors')
+        self.lights_button = self.findChild(QtWidgets.QPushButton, 'Lights')
+        self.announceStations_button = self.findChild(QtWidgets.QPushButton, 'Announcements')
+        self.ads_button = self.findChild(QtWidgets.QPushButton, 'Advertisements')
+        self.automaticMode_button = self.findChild(QtWidgets.QPushButton, 'AutomaticMode')
+        self.manualMode_button = self.findChild(QtWidgets.QPushButton, 'ManualMode')
+        self.defineToggles()
+
+        #Define buttons on Information Page
+        self.return_button2 = self.findChild(QtWidgets.QPushButton, 'MainMenu3')
+
+    def defineToggles(self):
+        # Give buttons capability to toggle in appearance
+        self.doors_button.setCheckable(True)
+        self.doors_button.setStyleSheet("QPushButton{background-color:green;}QPushButton:checked{background-color:rgb(255, 51, 16);}")
+        self.lights_button.setCheckable(True)
+        self.lights_button.setStyleSheet("QPushButton{background-color:green;}QPushButton:checked{background-color:rgb(255, 51, 16);}")
+        self.announceStations_button.setCheckable(True)
+        self.announceStations_button.setStyleSheet("QPushButton{background-color:green;}QPushButton:checked{background-color:rgb(255, 51, 16);}")
+        self.ads_button.setCheckable(True)
+        self.ads_button.setStyleSheet("QPushButton{background-color:green;}QPushButton:checked{background-color:rgb(255, 51, 16);}")
+        self.automaticMode_button.setCheckable(True)
+        self.automaticMode_button.setStyleSheet("QPushButton{background-color:green;}QPushButton:checked{background-color:rgb(255, 51, 16);}")
+        self.manualMode_button.setCheckable(True)
+        self.manualMode_button.setStyleSheet("QPushButton{background-color:rgb(255, 51, 16);}QPushButton:checked{background-color:green;}")
+
+
+    def set_button_state(self, index):
+        self.button1.setEnabled(True)
+        self.button2.setEnabled(True)
+        self.return_button1.setEnabled(True)
+        self.return_button2.setEnabled(True)
 
     def trainActions(self):
         # This is executed when the button is pressed
@@ -36,5 +83,5 @@ class Ui(QtWidgets.QMainWindow):
         app.exit()
 
 app = QtWidgets.QApplication(sys.argv)
-windows = Ui()
+windows = SWTrainUi()
 app.exec_()
