29d1039c974d21c394ddb18e8992d43261c668fc
Evan Kutney <43249484+EvanKutney@users.noreply.github.com>
2020-10-07 18:44:58 -0400

    Completed UI
    Skeleton of completed UI, no actual data yet but all the components are there

--- src/TrackModel/gui.py
+++ src/TrackModel/gui.py
@@ -78,14 +78,53 @@ class Ui(QtWidgets.QMainWindow):
 
 
         self.heater_button_1 = self.findChild(QtWidgets.QPushButton, 'heater_button_1')
-        self.heater_button_1.clicked.connect(self.heater1)
-
-        self.heater_button_2.clicked.connect(self.heater2)
         self.heater_button_2 = self.findChild(QtWidgets.QPushButton, 'heater_button_2')
-
-        self.heater_button_3.clicked.connect(self.heater3)
         self.heater_button_3 = self.findChild(QtWidgets.QPushButton, 'heater_button_3')
+        self.heater_button_4 = self.findChild(QtWidgets.QPushButton, 'heater_button_4')
+        self.heater_button_5 = self.findChild(QtWidgets.QPushButton, 'heater_button_5')
+        self.heater_button_6 = self.findChild(QtWidgets.QPushButton, 'heater_button_6')
+        self.heater_button_7 = self.findChild(QtWidgets.QPushButton, 'heater_button_7')
+        self.heater_button_8 = self.findChild(QtWidgets.QPushButton, 'heater_button_8')
+        self.heater_button_9 = self.findChild(QtWidgets.QPushButton, 'heater_button_9')
+        self.heater_button_10 = self.findChild(QtWidgets.QPushButton, 'heater_button_10')
+        self.heater_button_11 = self.findChild(QtWidgets.QPushButton, 'heater_button_11')
+        self.heater_button_12 = self.findChild(QtWidgets.QPushButton, 'heater_button_12')
+        self.heater_button_13 = self.findChild(QtWidgets.QPushButton, 'heater_button_13')
+        self.heater_button_14 = self.findChild(QtWidgets.QPushButton, 'heater_button_14')
+        self.heater_button_15 = self.findChild(QtWidgets.QPushButton, 'heater_button_15')
 
+        self.heater_button_1.clicked.connect(self.heater1)
+        self.heater_button_2.clicked.connect(self.heater2)
+        self.heater_button_3.clicked.connect(self.heater3)
+        self.heater_button_4.clicked.connect(self.heater4)
+        self.heater_button_5.clicked.connect(self.heater5)
+        self.heater_button_6.clicked.connect(self.heater6)
+        self.heater_button_7.clicked.connect(self.heater7)
+        self.heater_button_8.clicked.connect(self.heater8)
+        self.heater_button_9.clicked.connect(self.heater9)
+        self.heater_button_10.clicked.connect(self.heater10)
+        self.heater_button_11.clicked.connect(self.heater11)
+        self.heater_button_12.clicked.connect(self.heater12)
+        self.heater_button_13.clicked.connect(self.heater13)
+        self.heater_button_14.clicked.connect(self.heater14)
+        self.heater_button_15.clicked.connect(self.heater15)
+        
+
+        self.back_button_1.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.back_button_2.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.back_button_3.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.back_button_4.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.back_button_5.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.back_button_6.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.back_button_7.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.back_button_8.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.back_button_9.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.back_button_10.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.back_button_11.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.back_button_12.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.back_button_13.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.back_button_14.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+        self.back_button_15.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
 
         self.show()
 
@@ -107,14 +146,53 @@ class Ui(QtWidgets.QMainWindow):
         self.block15 = self.findChild(QtWidgets.QPushButton, 'pushButton_15')
         self.stacked_widget = self.findChild(QtWidgets.QStackedWidget, 'stackedWidget')
     
-        self.heater_button_1 = self.findChild(QtWidgets.QPushButton, 'heater_button_1')
-        self.heater_button_2 = self.findChild(QtWidgets.QPushButton, 'heater_button_2')
-        self.heater_button_3 = self.findChild(QtWidgets.QPushButton, 'heater_button_3')
+        #self.heater_button_1 = self.findChild(QtWidgets.QPushButton, 'heater_button_1')
+        #self.heater_button_2 = self.findChild(QtWidgets.QPushButton, 'heater_button_2')
+        #self.heater_button_3 = self.findChild(QtWidgets.QPushButton, 'heater_button_3')
+        #self.heater_button_1 = self.findChild(QtWidgets.QPushButton, 'heater_button_4')
+        #self.heater_button_2 = self.findChild(QtWidgets.QPushButton, 'heater_button_5')
+        #self.heater_button_3 = self.findChild(QtWidgets.QPushButton, 'heater_button_6')
+        #self.heater_button_1 = self.findChild(QtWidgets.QPushButton, 'heater_button_7')
+        #self.heater_button_2 = self.findChild(QtWidgets.QPushButton, 'heater_button_8')
+        #self.heater_button_3 = self.findChild(QtWidgets.QPushButton, 'heater_button_9')
+        #self.heater_button_1 = self.findChild(QtWidgets.QPushButton, 'heater_button_10')
+        #self.heater_button_2 = self.findChild(QtWidgets.QPushButton, 'heater_button_11')
+        #self.heater_button_3 = self.findChild(QtWidgets.QPushButton, 'heater_button_12')
+        #self.heater_button_1 = self.findChild(QtWidgets.QPushButton, 'heater_button_13')
+        #self.heater_button_2 = self.findChild(QtWidgets.QPushButton, 'heater_button_14')
+        #self.heater_button_3 = self.findChild(QtWidgets.QPushButton, 'heater_button_15')
 
+        self.back_button_1 = self.findChild(QtWidgets.QPushButton, 'back_button_1')
+        self.back_button_2 = self.findChild(QtWidgets.QPushButton, 'back_button_2')
+        self.back_button_3 = self.findChild(QtWidgets.QPushButton, 'back_button_3')
+        self.back_button_4 = self.findChild(QtWidgets.QPushButton, 'back_button_4')
+        self.back_button_5 = self.findChild(QtWidgets.QPushButton, 'back_button_5')
+        self.back_button_6 = self.findChild(QtWidgets.QPushButton, 'back_button_6')
+        self.back_button_7 = self.findChild(QtWidgets.QPushButton, 'back_button_7')
+        self.back_button_8 = self.findChild(QtWidgets.QPushButton, 'back_button_8')
+        self.back_button_9 = self.findChild(QtWidgets.QPushButton, 'back_button_9')
+        self.back_button_10 = self.findChild(QtWidgets.QPushButton, 'back_button_10')
+        self.back_button_11 = self.findChild(QtWidgets.QPushButton, 'back_button_11')
+        self.back_button_12 = self.findChild(QtWidgets.QPushButton, 'back_button_12')
+        self.back_button_13 = self.findChild(QtWidgets.QPushButton, 'back_button_13')
+        self.back_button_14 = self.findChild(QtWidgets.QPushButton, 'back_button_14')
+        self.back_button_15 = self.findChild(QtWidgets.QPushButton, 'back_button_15')
 
         h_index1 = 0
         h_index2 = 0
         h_index3 = 0
+        h_index4 = 0
+        h_index5 = 0
+        h_index6 = 0
+        h_index7 = 0
+        h_index8 = 0
+        h_index9 = 0
+        h_index10 = 0
+        h_index11 = 0
+        h_index12 = 0
+        h_index13 = 0
+        h_index14 = 0
+        h_index15 = 0
     def page0(self):
         #new_index = self.stacked_widget.currentIndex()-1
         #if new_index >= 0:
@@ -198,7 +276,116 @@ class Ui(QtWidgets.QMainWindow):
             self.heater_button_3.setStyleSheet("background-color : green")
         else:
             self.heater_button_3.setText('Off')
-            self.heater_button_3.setStyleSheet("background-color : rgb(255,0,0)")                     
+            self.heater_button_3.setStyleSheet("background-color : rgb(255,0,0)") 
+
+    def heater4(self, h_index4):
+        h_index4 = h_index4 + 1;
+        if (h_index4 % 2 == 0):
+            self.heater_button_4.setText('On')
+            self.heater_button_4.setStyleSheet("background-color : green")
+        else:
+            self.heater_button_4.setText('Off')
+            self.heater_button_4.setStyleSheet("background-color : rgb(255,0,0)") 
+
+    def heater5(self, h_index5):
+        h_index5 = h_index5 + 1;
+        if (h_index5 % 2 == 0):
+            self.heater_button_5.setText('On')
+            self.heater_button_5.setStyleSheet("background-color : green")
+        else:
+            self.heater_button_5.setText('Off')
+            self.heater_button_5.setStyleSheet("background-color : rgb(255,0,0)")
+
+    def heater6(self, h_index6):
+        h_index6 = h_index6 + 1;
+        if (h_index6 % 2 == 0):
+            self.heater_button_6.setText('On')
+            self.heater_button_6.setStyleSheet("background-color : green")
+        else:
+            self.heater_button_6.setText('Off')
+            self.heater_button_6.setStyleSheet("background-color : rgb(255,0,0)")
+
+    def heater7(self, h_index7):
+        h_index7 = h_index7 + 1;
+        if (h_index7 % 2 == 0):
+            self.heater_button_7.setText('On')
+            self.heater_button_7.setStyleSheet("background-color : green")
+        else:
+            self.heater_button_7.setText('Off')
+            self.heater_button_7.setStyleSheet("background-color : rgb(255,0,0)")
+
+    def heater8(self, h_index8):
+        h_index8 = h_index8 + 1;
+        if (h_index8 % 2 == 0):
+            self.heater_button_8.setText('On')
+            self.heater_button_8.setStyleSheet("background-color : green")
+        else:
+            self.heater_button_8.setText('Off')
+            self.heater_button_8.setStyleSheet("background-color : rgb(255,0,0)")
+
+    def heater9(self, h_index9):
+        h_index9 = h_index9 + 1;
+        if (h_index9 % 2 == 0):
+            self.heater_button_9.setText('On')
+            self.heater_button_9.setStyleSheet("background-color : green")
+        else:
+            self.heater_button_9.setText('Off')
+            self.heater_button_9.setStyleSheet("background-color : rgb(255,0,0)")
+
+    def heater10(self, h_index10):
+        h_index10 = h_index10 + 1;
+        if (h_index10 % 2 == 0):
+            self.heater_button_10.setText('On')
+            self.heater_button_10.setStyleSheet("background-color : green")
+        else:
+            self.heater_button_10.setText('Off')
+            self.heater_button_10.setStyleSheet("background-color : rgb(255,0,0)")
+
+    def heater11(self, h_index11):
+        h_index11 = h_index11 + 1;
+        if (h_index11 % 2 == 0):
+            self.heater_button_11.setText('On')
+            self.heater_button_11.setStyleSheet("background-color : green")
+        else:
+            self.heater_button_11.setText('Off')
+            self.heater_button_11.setStyleSheet("background-color : rgb(255,0,0)")
+
+    def heater12(self, h_index12):
+        h_index12 = h_index12 + 1;
+        if (h_index12 % 2 == 0):
+            self.heater_button_12.setText('On')
+            self.heater_button_12.setStyleSheet("background-color : green")
+        else:
+            self.heater_button_12.setText('Off')
+            self.heater_button_12.setStyleSheet("background-color : rgb(255,0,0)")
+
+    def heater13(self, h_index13):
+        h_index13 = h_index13 + 1;
+        if (h_index13 % 2 == 0):
+            self.heater_button_13.setText('On')
+            self.heater_button_13.setStyleSheet("background-color : green")
+        else:
+            self.heater_button_13.setText('Off')
+            self.heater_button_13.setStyleSheet("background-color : rgb(255,0,0)")
+
+    def heater14(self, h_index14):
+        h_index14 = h_index14 + 1;
+        if (h_index14 % 2 == 0):
+            self.heater_button_14.setText('On')
+            self.heater_button_14.setStyleSheet("background-color : green")
+        else:
+            self.heater_button_14.setText('Off')
+            self.heater_button_14.setStyleSheet("background-color : rgb(255,0,0)")
+
+    def heater15(self, h_index15):
+        h_index15 = h_index15 + 1;
+        if (h_index15 % 2 == 0):
+            self.heater_button_15.setText('On')
+            self.heater_button_15.setStyleSheet("background-color : green")
+        else:
+            self.heater_button_15.setText('Off')
+            self.heater_button_15.setStyleSheet("background-color : rgb(255,0,0)")
+                                
 
     #def insert_page(self, widget, index=-1):
         #self.stacked_widget.insertWidget(index, widget)
