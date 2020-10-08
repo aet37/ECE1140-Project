2ea46755a909121df49eb2b8c30151db934bf30c
Collin Hough <chough724@gmail.com>
2020-10-08 12:44:12 -0400

    UI updates

--- src/UI/SWTrainController/TrainController.py
+++ src/UI/SWTrainController/TrainController.py
@@ -1,21 +1,30 @@
 import os
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import QPushButton, QStackedWidget
+from PyQt5.QtCore import QTimer
 import sys
+sys.path.insert(1, 'src/UI')
+from server_functions import *
 
 class SWTrainUi(QtWidgets.QMainWindow):
     def __init__(self):
         super(SWTrainUi, self).__init__()
-        uic.loadUi('/Users/collinhough/Documents/Github/ECE1140-Project/src/SWTrainController/UI/TrainController.ui', self)
+        uic.loadUi('src/UI/SWTrainController/TrainController.ui', self)
 
+        # Create timer to update pages
+        self.train_actions_timer = QTimer()
+        self.train_actions_timer.timeout.connect(self.update_data)
+
+        # Initialize all buttons and the page of the UI
         self.initUI()
         self.stacked_widget.currentChanged.connect(self.set_button_state)
         self.stacked_widget.setCurrentIndex(0)
 
         # Define which pages buttons will take you to
         self.button1.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(1))
+        # Start timer to update train actions page
+        self.stopAllTimers()
+        self.train_actions_timer.start(250)
+
         self.button2.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(2))
         self.return_button1.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
         self.return_button2.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
@@ -65,22 +74,25 @@ class SWTrainUi(QtWidgets.QMainWindow):
         self.manualMode_button.setCheckable(True)
         self.manualMode_button.setStyleSheet("QPushButton{background-color:rgb(255, 51, 16);}QPushButton:checked{background-color:green;}")
 
-
     def set_button_state(self, index):
         self.button1.setEnabled(True)
         self.button2.setEnabled(True)
         self.return_button1.setEnabled(True)
         self.return_button2.setEnabled(True)
 
-    def trainActions(self):
-        # This is executed when the button is pressed
-        app.exit()
-    def information(self):
-        # This is executed when the button is pressed
-        app.exit()
     def logout(self):
         # This is executed when the button is pressed
         app.exit()
+
+    # Define function to update displayed data
+    def update_data(self):
+        responseCode, speed = send_message(RequestCode.GET_COMMAND_SPEED)
+        if responseCode == ResponseCode.SUCCESS:
+            self.CommandSpeedLabel.setText(speed + " MPH")
+
+
+    def stopAllTimers(self):
+        self.train_actions_timer.stop()
 
 app = QtWidgets.QApplication(sys.argv)
 windows = SWTrainUi()
