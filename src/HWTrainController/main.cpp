d685f017e26960cff09a3a2b1278390b2eb13fd7
EC2 Default User <ec2-user@ip-172-31-46-198.us-east-2.compute.internal>
2020-10-08 19:41:12 +0000

    Fix build

--- src/HWTrainController/main.cpp
+++ src/HWTrainController/main.cpp
@@ -6,7 +6,3 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-
-int main(){
-    // If button is pressed to change doors or lights or what have you, then if it is a bool, the main will change said bool value automatically whereas if it is an int then the user would
-    // use the joystick to change the value to whatever they deem fit.
