811b549a329db6c31b25fd14730fdfe2615b0328
aet37 <aet37@pitt.edu>
2020-10-12 12:46:51 -0400

    Add Catch Tests to CTC

---
+++ tests/UnitTests/CTC/CTCDefUT.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file RequestManagerRepositoryUT.cpp
+*/
+
+// SYSTEM INCLUDES
+// (None)
+
+// C++ PROJECT INCLUDES
+#include <catch2/catch.hpp>
+#include "CTCDef.hpp"
+
+/*
+ * Test Train Struct Constructor
+ */
+TEST_CASE( "Test Train Struct Constructor", "[Train::Train()]" )
+{
+	Train ta1(3, 10);
+
+	REQUIRE(ta1.train_id == 3);
+	REQUIRE(ta1.destination_block == 10);
+}
+
+/*
+ * Test Track Struct Constructor
+ */
+TEST_CASE( "Test Track Struct Constructor", "[Track::Track()]" )
+{
+	Track tr1;
+
+	REQUIRE(tr1.open);  // Track initialized to open
+	REQUIRE(!tr1.occupied);  // Track initialized to
+}
+
+TEST_CASE( "Test Signal Struct Constructor", "[Signal::Signal()]" )
+{
+	Signal s1;
+
+	REQUIRE(s1.status == LIGHT_RED);
+	REQUIRE(s1.track_on == nullptr);
+}
