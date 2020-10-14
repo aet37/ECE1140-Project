b362f2f3ea3172e2b4023079ad6d658063b883aa
Luke Karavolis <lukejrkaravolis@gmail.com>
2020-10-14 15:39:28 -0400

    Pylint errors

--- src/UI/login_gui.py
+++ src/UI/login_gui.py
@@ -1,43 +1,40 @@
+"""Main page displayed to the user when they start the application"""
 
 import os
-from PyQt5 import QtWidgets, uic
 import sys
+from PyQt5 import QtWidgets, uic
 
-#import CTC
-
-username = "trainmodel"
-password = "jerry"
-
-# GLOBALS
-class Ui(QtWidgets.QMainWindow):
+class LoginPage(QtWidgets.QMainWindow):
+    """Page shown to user upon application startup"""
     def __init__(self):
-        super(Ui, self).__init__()
+        super().__init__()
         self.login()
 
-
     def login(self):
+        """Loads the login page ui and displays it"""
         uic.loadUi('src/UI/Login_Page.ui', self)
         self.alert_login = self.findChild(QtWidgets.QLabel, 'alert_login')
         self.username_in = self.findChild(QtWidgets.QLineEdit, 'username_in')
         self.password_in = self.findChild(QtWidgets.QLineEdit, 'password_in')
 
         self.button = self.findChild(QtWidgets.QPushButton, 'login_button')# Find the button
-        self.button.clicked.connect(self.loginParse)
+        self.button.clicked.connect(self.login_parse)
         self.button = self.findChild(QtWidgets.QPushButton, 'TurnOff') # Find the button
-        self.button.clicked.connect(self.Leave)
+        self.button.clicked.connect(leave)
         self.show()
 
-    def loginParse(self):
+    def login_parse(self):
+        """Checks the user's credentials and starts the specific module's ui if correct"""
         username = self.username_in.text()
         password = self.password_in.text()
         if username == "trainmodel" and password == "jerry":
-            if(sys.platform == 'darwin'):
+            if sys.platform == 'darwin':
                 os.system('python3 src/UI/TrainModel/trainmodel_gui.py &')
             else:
                 os.system('start /B python src/UI/TrainModel/trainmodel_gui.py')
             app.exit()
         elif username == "trackmodel" and password == "jerry":
-            if(sys.platform == 'darwin'):
+            if sys.platform == 'darwin':
                 os.system('python3 src/UI/TrackModel/trackmodel_gui.py &')
             else:
                 os.system('start /B python src/UI/TrackModel/trackmodel_gui.py')
@@ -47,7 +44,7 @@ class Ui(QtWidgets.QMainWindow):
         elif username == "swtrack" and password == "jerry":
             print("swtrack")
         elif username == "ctc" and password == "jerry":
-            if(sys.platform == 'darwin'):
+            if sys.platform == 'darwin':
                 os.system('python3 src/UI/CTC/ctc_gui.py &')
             else:
                 os.system('start /B python src/UI/CTC/ctc_gui.py')
@@ -55,13 +52,13 @@ class Ui(QtWidgets.QMainWindow):
         elif username == "hwtrain" and password == "jerry":
             print("hwtrain")
         elif username == "swtrain" and password == "jerry":
-            if(sys.platform == 'darwin'):
+            if sys.platform == 'darwin':
                 os.system('python3 src/UI/SWTrainController/TrainController.py &')
             else:
                 os.system('start /B python src/UI/TrainController/TrainController.py')
             app.exit()
         elif username == "engineer" and password == "jerry":
-            if(sys.platform == 'darwin'):
+            if sys.platform == 'darwin':
                 os.system('python3 src/UI/SWTrainController/TrainEngineer.py &')
             else:
                 os.system('start /B python src/UI/TrainController/TrainEngineer.py')
@@ -69,11 +66,11 @@ class Ui(QtWidgets.QMainWindow):
         else:
             self.alert_login.setStyleSheet("color: red;")
 
-    def Leave(self):
-    	# Exit application
-    	app.exit()
+def leave():
+    """Closes the page"""
+    app.exit()
 
 # Main Login Screen
 app = QtWidgets.QApplication(sys.argv)
-window = Ui()
-app.exec_()         # Exit from login screen
+window = LoginPage()
+app.exec_()
