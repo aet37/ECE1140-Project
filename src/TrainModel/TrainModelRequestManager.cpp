a22358c2d95da8430d2e1581659324334c192bf0
Luke Karavolis <lukejrkaravolis@gmail.com>
2020-10-15 11:35:31 -0400

    Create TrainModel stuff

---
+++ src/TrainModel/TrainModelRequestManager.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file TrainModelRequestManager.cpp
+ *
+ * @brief Implementation of RequestManager class for TrainModel Module
+*/
+
+// SYSTEM INCLUDES
+// (None)
+
+// C++ PROJECT INCLUDES
+#include "TrainModelRequestManager.hpp" // Header for class
+#include "Request.hpp" // For Request
+#include "Response.hpp" // For Response
+#include "Logger.hpp"   // For LOG macros
+
+namespace TrainModel
+{
+
+void TrainModelRequestManager::HandleRequest(const Common::Request& rRequest, Common::Response& rResponse)
+{
+    LOG_TRAIN_MODEL("RequestCode = %d", static_cast<uint8_t>(rRequest.GetRequestCode()));
+    switch (rRequest.GetRequestCode())
+    {
+        default:
+            std::cerr << "Invalid command " << static_cast<uint16_t>(rRequest.GetRequestCode())
+                      << " received" << std::endl;
+            rResponse.SetResponseCode(Common::ResponseCode::ERROR);
+            return;
+    }
+}
+
+} // namespace TrainModel
