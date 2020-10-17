f50d2163abf00a31e77232f6dc29705fa95ea9dc
Luke Karavolis <karavolisl@gmail.com>
2020-10-17 18:55:42 -0400

    Switch to service queue (#83)
    * Change to service queue
    
    * Fix build
    
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-46-198.us-east-2.compute.internal>

--- src/HWTrackController/HWTrackControllerMain.cpp
+++ src/HWTrackController/HWTrackControllerMain.cpp
@@ -7,6 +7,7 @@
 
 // C++ PROJECT INCLUDES
 #include "HWTrackControllerMain.hpp" // Header for functions
+#include "Assert.hpp" // For ASSERT
 #include "Logger.hpp" // For LOG macros
 
 namespace HWTrackController
@@ -17,6 +18,23 @@ Common::ServiceQueue<Common::Request> serviceQueue;
 void moduleMain()
 {
     LOG_HW_TRACK_CONTROLLER("Thread starting...");
+
+    while (true)
+    {
+        Common::Request receivedRequest = serviceQueue.Pop();
+
+        switch (receivedRequest.GetRequestCode())
+        {
+            case Common::RequestCode::SWTRACK_GET_TRACK_SIGNAL:
+            {
+                
+                break;
+            }
+            default:
+                ASSERT(false, "Unexpected request received %d", static_cast<uint16_t>(receivedRequest.GetRequestCode()));
+                break;
+        }
+    }
 }
 
 } // namespace HWTrackController
