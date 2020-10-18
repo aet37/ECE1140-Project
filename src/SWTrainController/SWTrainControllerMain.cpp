93d658a2088c8ccc3292d21384c64260e6e4bcd5
Luke Karavolis <lukejrkaravolis@gmail.com>
2020-10-17 22:33:28 -0400

    Remove temp stuff

--- src/SWTrainController/SWTrainControllerMain.cpp
+++ src/SWTrainController/SWTrainControllerMain.cpp
@@ -18,14 +18,6 @@ Common::ServiceQueue<Common::Request> serviceQueue;
 void moduleMain()
 {
     LOG_SW_TRAIN_CONTROLLER("Thread starting...");
-
-    Common::Request req(Common::RequestCode::TRAIN_MODEL_GIVE_POWER, "5");
-
-    TrainModel::serviceQueue.Push(req);
-
-    Common::Request receivedReq = serviceQueue.Pop();
-
-    LOG_TRAIN_MODEL("We received something!!! %s", receivedReq.GetData().c_str());
 }
 
 } // namespace SWTrainController
