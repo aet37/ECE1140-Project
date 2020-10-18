f4516bfde80e8c7c86dcb807de9e3e6e52b5ecd9
Luke Karavolis <karavolisl@gmail.com>
2020-10-17 22:37:33 -0400

    Simple power and current speed (#84)
    * Initial attempt
    
    * Fix build
    
    * Remove temp stuff
    
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-46-198.us-east-2.compute.internal>

--- include/TrainModel/TrainModelData.hpp
+++ include/TrainModel/TrainModelData.hpp
@@ -31,25 +31,6 @@ int getPower()
 }
 
 int tm_train_id, tm_authority, tm_command_speed, tm_current_speed, tm_speed_limit;
-/**
- * @brief	Buffer function to send info about new train from CTC to Track Controller
- *
- * @param[in]	train_id
- * @param[in]	authority
- * @param[in]	command_speed
- * @param[in]   current_speed
- * @param[in]   speed_limit
- *
- * @return	None
- */
-void TrainInfoBuffer_TrainModel(int train_id, int authority, int command_speed, int current_speed, int speed_limit)
-{
-	tm_train_id = train_id;
-	tm_authority = authority;
-	tm_command_speed = command_speed;
-	tm_current_speed = current_speed;
-	tm_speed_limit = speed_limit;
-}
 
 int getID()
 {
--- src/TrainModel/TrainModelMain.cpp
+++ src/TrainModel/TrainModelMain.cpp
@@ -7,6 +7,9 @@
 
 // C++ PROJECT INCLUDES
 #include "TrainModelMain.hpp" // Header for functions
+#include "SWTrainControllerMain.hpp" // For SWTrainController::serviceQueue
+#include "TrainModelData.hpp" // For TrainModel::tm_current_speed
+#include "Assert.hpp" // For ASSERT
 #include "Logger.hpp" // For LOG macros
 
 namespace TrainModel
@@ -17,6 +20,27 @@ Common::ServiceQueue<Common::Request> serviceQueue;
 void moduleMain()
 {
     LOG_TRAIN_MODEL("Thread starting...");
+
+    while (true)
+    {
+        Common::Request receivedRequest = serviceQueue.Pop();
+
+        switch (receivedRequest.GetRequestCode())
+        {
+            case Common::RequestCode::TRAIN_MODEL_GIVE_POWER:
+            {
+                uint32_t power = std::stoi(receivedRequest.GetData());
+                uint32_t currentSpeed = (power * 2);
+                tm_current_speed = currentSpeed;
+                std::string currentSpeedSend = std::to_string(currentSpeed);
+                Common::Request newRequest(Common::RequestCode::SW_TRAIN_CONTROLLER_GET_CURRENT_SPEED, currentSpeedSend);
+                SWTrainController::serviceQueue.Push(newRequest);
+                break;
+            }
+            default:
+                ASSERT(false, "Unexpected request code %d", static_cast<uint16_t>(receivedRequest.GetRequestCode()));
+        }
+    }
 }
 
 } // namespace TrainModel
