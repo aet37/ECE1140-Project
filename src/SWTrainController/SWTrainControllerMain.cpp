37e2d95d36eb7d4bee7500662dd4c0ccda4fa1b1
Luke Karavolis <lukejrkaravolis@gmail.com>
2020-10-17 22:17:50 -0400

    Initial attempt

--- src/SWTrainController/SWTrainControllerMain.cpp
+++ src/SWTrainController/SWTrainControllerMain.cpp
@@ -7,6 +7,7 @@
 
 // C++ PROJECT INCLUDES
 #include "SWTrainControllerMain.hpp" // Header for functions
+#include "TrainModelMain.hpp"
 #include "Logger.hpp" // For LOG macros
 
 namespace SWTrainController
@@ -17,6 +18,14 @@ Common::ServiceQueue<Common::Request> serviceQueue;
 void moduleMain()
 {
     LOG_SW_TRAIN_CONTROLLER("Thread starting...");
+
+    Common::Request req(TRAIN_MODEL_GIVE_POWER, "5");
+
+    TrainModel::serviceQueue.Push(req);
+
+    serviceQueue.Pop();
+
+    LOG_TRAIN_MODEL("We received something!!!");
 }
 
 } // namespace SWTrainController
