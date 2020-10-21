b45a4fb05bf413e9bed340fef7ebd03b89f6a990
tys29 <tyler.sheet@gmail.com>
2020-10-21 17:02:25 -0400

    commit on 'tylers-stuff'

---
+++ include/HWTrainController/Trainfunctions.h
@@ -0,0 +1,21 @@
+#ifndef Trainfunctions_h
+#define Trainfunctions_h
+
+
+
+class Trainfunctions{
+	private:
+		bool mode, trackSignal, emergencyBrake;
+		int serviceBrake;
+	public:
+		void setMode(int);
+		void setTrackSignal(int);
+		void setServiceBrake(int);
+		void setEmergencyBrake(int);
+		int getMode();
+		int getTrackSignal();
+		int getServiceBrake();
+		int getEmergencyBrake();	
+};
+
+#endif
