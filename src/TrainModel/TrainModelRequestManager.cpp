f6c0ba1dd9d7190a34c9ab7bfdf4af5b8ee4db56
Kenny Meier <59569725+kem243@users.noreply.github.com>
2020-11-04 22:07:58 -0500

    type a nice message (#109)
    * update parameters of the train model GUI
    
    * The rest of the parameters may work
    
    * Pylint!!!
    
    * Update TrainModelMain.cpp
    
    * SET_THE_DAMN_LIGHTS
    
    SET_THE_DAMN_LIGHTS
    
    * rResponse.SetResponseCode(Common::ResponseCode::SUCCESS);
    
    rResponse.SetResponseCode(Common::ResponseCode::SUCCESS);
    
    Co-authored-by: Luke Karavolis <lukejrkaravolis@gmail.com>

--- src/TrainModel/TrainModelRequestManager.cpp
+++ src/TrainModel/TrainModelRequestManager.cpp
@@ -12,7 +12,7 @@
 #include "Request.hpp" // For Request
 #include "Response.hpp" // For Response
 #include "Logger.hpp"   // For LOG macros
-#include "TrainModelData.hpp"
+#include "TrainModelMain.hpp" // For TrainModel::serviceQueue
 
 namespace TrainModel
 {
@@ -25,6 +25,15 @@ void TrainModelRequestManager::HandleRequest(const Common::Request& rRequest, Co
         case Common::RequestCode::TRAIN_MODEL_GUI_GATHER_DATA:
             // Temporarily hard code the current speed
             rResponse.SetData("10");
+            rResponse.SetResponseCode(Common::ResponseCode::SUCCESS);
+            break;
+        case Common::RequestCode::TRAIN_MODEL_GUI_SET_TRAIN_LENGTH:
+        case Common::RequestCode::TRAIN_MODEL_GUI_SET_TRAIN_MASS:
+        case Common::RequestCode::TRAIN_MODEL_GUI_SET_TRAIN_HEIGHT:
+        case Common::RequestCode::TRAIN_MODEL_GUI_SET_TRAIN_PASSENGER_COUNT:
+        case Common::RequestCode::TRAIN_MODEL_GUI_SET_TRAIN_WIDTH:
+        case Common::RequestCode::TRAIN_MODEL_GUI_SET_TRAIN_CREW_COUNT:
+            TrainModel::serviceQueue.Push(rRequest);
             rResponse.SetResponseCode(Common::ResponseCode::SUCCESS);
             break;
         default:
