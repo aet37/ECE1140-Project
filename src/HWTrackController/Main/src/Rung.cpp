44909e20d10049ef4265d396936bca8c4b5af38e
Luke Karavolis <lukejrkaravolis@gmail.com>
2020-11-05 22:51:39 -0500

    OTE instruction and test

--- src/HWTrackController/Main/src/Rung.cpp
+++ src/HWTrackController/Main/src/Rung.cpp
@@ -11,6 +11,7 @@
 #include "../include/Rung.hpp" // Header for class
 #include "../include/ArduinoLogger.hpp" // For LOG
 #include "../include/Instruction.hpp" // For Instruction
+#include "../include/TagDatabase.hpp" // For TagDatabase
 
 void Rung::AddInstruction(Instruction* pInst)
 {
@@ -22,13 +23,21 @@ void Rung::Execute()
 {
     // Execute the instructions on this rung. If an instruction evaluates to false,
     // We should stop executing this rung
+    bool ret = true;
     for (int i = 0; i < m_instructions.GetLength(); i++)
     {
-        LOG("Executing instruction "); LOG_DECN(i);
-        bool ret = m_instructions[i]->Evaluate();
-        if (ret == false)
+        if (ret == true)
         {
-            break;
+            LOG("Executing instruction "); LOG_DECN(i);
+            ret = m_instructions[i]->Evaluate();
+        }
+        else
+        {
+            // Check for OTE instructions and set their associated tags low
+            if (m_instructions[i]->GetInstructionType() == InstructionType::OTE)
+            {
+                (void)TagDatabase::SetTag(m_instructions[i]->GetArgument(), false);
+            }
         }
     }
 }
