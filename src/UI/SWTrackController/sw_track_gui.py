35759bf39fcfa1db38b79c5bbd3f52c1bb4d3c5a
Luke Karavolis <karavolisl@gmail.com>
2020-11-07 15:59:30 -0500

    Compiler Error Handling (#124)
    * Update exception
    
    * Added error handling

--- src/UI/SWTrackController/sw_track_gui.py
+++ src/UI/SWTrackController/sw_track_gui.py
@@ -1,19 +1,23 @@
+"""Software Track Controller GUI"""
 
 import os
+import sys
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QFileDialog
-import sys
 
 sys.path.insert(1, 'src')
-from SWTrackController.Compiler.lexer import Lexer
+from SWTrackController.Compiler.lexer import CompilationError, Lexer
 from SWTrackController.Compiler.emitter import Emitter
 from SWTrackController.Compiler.parse import Parser
 
 from UI.server_functions import RequestCode, send_message
 
+from UI.Common.common import Alert
+
 class SWTrackControllerUi(QtWidgets.QMainWindow):
+    """GUI for the track controller module"""
     def __init__(self):
-        super(SWTrackControllerUi, self).__init__()
+        super().__init__()
         uic.loadUi('src/UI/SWTrackController/track_controller.ui', self)
 
         # Current track controller and block selected
@@ -43,10 +47,13 @@ class SWTrackControllerUi(QtWidgets.QMainWindow):
         file_name = dialog.selectedFiles()
         output_file = self.compile_program(file_name[0])
 
-        if output_file != None:
+        if output_file is not None:
             self.send_compiled_program(output_file)
+            alert = Alert("Program downloaded successfully!")
+            alert.exec_()
 
-    def compile_program(self, file_name):
+    @staticmethod
+    def compile_program(file_name):
         """Method used to invoke the PLC language compiler on the given file
 
         :param str file_name: Absolute path to the file to compile
@@ -56,7 +63,7 @@ class SWTrackControllerUi(QtWidgets.QMainWindow):
         # Gather the source code from the file
         source_code = ''
         for line in open(file_name, 'r'):
-            source_code += line
+            source_code = ''.join([source_code, line])
 
         # Create compiler elements
         output_file = 'CompiledOutput.txt'
@@ -68,12 +75,13 @@ class SWTrackControllerUi(QtWidgets.QMainWindow):
         try:
             par.program(program_name=os.path.splitext(os.path.basename(file_name))[0])
             return output_file
-        except Exception as e:
-            print(e)
-            print("Compilation failed!")
+        except CompilationError as compilation_error:
+            alert = Alert("Compilation failed with error:\n {}".format(str(compilation_error)))
+            alert.exec_()
             return None
 
-    def send_compiled_program(self, output_file):
+    @staticmethod
+    def send_compiled_program(output_file):
         """Method used to read compiled program and send messages to the server
 
         :param str output_file: Name of the file containing the compiled program
