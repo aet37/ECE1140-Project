0200d366eae302dc18520f637f792922e444be9d
Luke Karavolis <lukejrkaravolis@gmail.com>
2020-11-09 01:16:29 -0500

    Added all track controllers to combo box

--- src/UI/SWTrackController/sw_track_gui.py
+++ src/UI/SWTrackController/sw_track_gui.py
@@ -4,6 +4,7 @@ import os
 import sys
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtCore import Qt
 
 sys.path.insert(1, 'src')
 from SWTrackController.Compiler.lexer import CompilationError, Lexer
@@ -12,13 +13,46 @@ from SWTrackController.Compiler.parse import Parser
 
 from UI.server_functions import RequestCode, send_message
 
-from UI.Common.common import Alert
+from UI.Common.common import Alert, Confirmation
 
 class SWTrackControllerUi(QtWidgets.QMainWindow):
     """GUI for the track controller module"""
     def __init__(self):
         super().__init__()
         uic.loadUi('src/UI/SWTrackController/track_controller.ui', self)
+
+        # All the track controllers and the blocks they control
+        self.red_line_controllers = [
+            [1, 2, 3, 4, 5, 6, 7, 8, 9], # Switch between C and D @ block 9
+            [0, 9, 10, 11, 12, 13, 14, 15],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], # Switch between A, E, and F @ block 16
+            [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26],
+            [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27], # Switch between H and T @ block 27
+            [72, 73, 74, 75, 76, 27, 28, 29, 30, 31, 32],
+            [72, 73, 74, 75, 76, 28, 29, 30, 31, 32, 33], # Switch between R and H @ block 33
+            [33, 34, 35, 36, 37],
+            [34, 35, 36, 37, 38], # Switch between H and Q @ block 38
+            [38, 39, 40, 41, 42, 43, 71, 70, 69, 68, 67],
+            [39, 40, 41, 42, 43, 44, 71, 70, 69, 68, 67], # Switch between O and H @ block 44
+            [44, 45, 46, 47, 48, 49, 50, 51],
+            [45, 46, 47, 48, 49, 50, 51, 52], # Switch between J and N @ block 52
+            [52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66]
+        ]
+
+        self.green_line_controllers = [
+            [0, 62, 61, 60, 59], # Switch between J, K, and Yard @ block 62
+            [62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76],
+            [63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 101] + list(range(102, 151)), # Switch between M, N, and R @ block 77
+            [77, 78, 79, 80, 81, 82, 83, 84],
+            [85, 78, 79, 80, 81, 82, 83, 84], # Switch between N, O, and Q @ block 85
+            [85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100],
+            list(range(101, 151)) + list(range(29, 58)), # Switch between Z, F, and G @ block 29
+            [29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14],
+            [28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13], # Switch between D, A, and C @ block 13
+            [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+            list(range(30, 59)), # Switch between I, J and Yard @ block 58
+            [58, 59, 60, 61]
+        ]
 
         # Current track controller and block selected
         self.current_track_controller = None
@@ -28,10 +62,41 @@ class SWTrackControllerUi(QtWidgets.QMainWindow):
         logout_button = self.findChild(QtWidgets.QPushButton, 'logout_button')
         logout_button.clicked.connect(SWTrackControllerUi.logout)
 
+        switch_position_button = self.findChild(QtWidgets.QPushButton, 'switch_position_button')
+        switch_position_button.setAttribute(Qt.WA_TranslucentBackground)
+        switch_position_button.clicked.connect(self.switch_position_button_clicked)
+
         download_program_button = self.findChild(QtWidgets.QPushButton, 'download_program_button')
         download_program_button.clicked.connect(self.download_program)
 
+        self.track_controller_combo_box = self.findChild(QtWidgets.QComboBox, 'track_controller_combo_box')
+        self.block_combo_box = self.findChild(QtWidgets.QComboBox, 'block_combo_box')
+
+        # Add all of the options for the red/green line
+        for i in range(0, len(self.red_line_controllers)):
+            self.track_controller_combo_box.addItem('Red Line - Controller #{}'.format(i + 1))
+        for i in range(0, len(self.green_line_controllers)):
+            self.track_controller_combo_box.addItem('Green Line - Controller #{}'.format(i + 1))
+        self.track_controller_combo_box.currentIndexChanged.connect(self.track_controller_selected)
+
+        # Select the default
+        self.track_controller_combo_box.setCurrentIndex(0)
+        self.track_controller_selected()
+
         self.show()
+
+    def track_controller_selected(self):
+        """Method called when a different track controller is selected"""
+        self.current_track_controller = self.track_controller_combo_box.currentText()[-1]
+
+        # Update the options in the block combo box
+        self.block_combo_box.clear()
+        if 'Red' in self.track_controller_combo_box.currentText():
+            for block in self.red_line_controllers[int(self.current_track_controller) - 1]:
+                self.block_combo_box.addItem("Block #{}".format(block))
+        else:
+            for block in self.green_line_controllers[int(self.current_track_controller) - 1]:
+                self.block_combo_box.addItem("Block #{}".format(block))
 
     def download_program(self):
         """Method called when the download program button is pressed"""
@@ -92,6 +157,14 @@ class SWTrackControllerUi(QtWidgets.QMainWindow):
             request_code = RequestCode[request_code]
 
             send_message(request_code, data)
+
+    def switch_position_button_clicked(self):
+        """Method called when the switch position button is pressed"""
+        confirmation = Confirmation("Are you sure you want to change the switch position?")
+
+        if confirmation.exec_():
+            pass
+            # TODO: Send a message indicating the switch has been switched
 
     @staticmethod
     def logout():
