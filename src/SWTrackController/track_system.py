9a2a0b7dfecb2c0673d3c190642ec02f6055c06e
Luke Karavolis <karavolisl@gmail.com>
2020-11-27 12:34:16 -0500

    Update GUI (#249)
    * Authority and stuff
    
    * Use polling for test
    
    * More updating pages
    
    * It's still not working :(
    
    * Temp fix for no plc program
    
    * Small fix
    
    * Add switch position button

--- src/SWTrackController/track_system.py
+++ src/SWTrackController/track_system.py
@@ -79,6 +79,10 @@ class TrackSystem:
         else:
             signals.update_red_switches.emit(switch_positions)
 
+        # TODO (ljk): Remove this once plc programs are autouploaded!
+        if authority is None:
+            authority = True
+
         # Pass the dispatch train information to the Track Model
         signals.trackmodel_dispatch_train.emit(train_id, destination_block, command_speed, authority, line, route)
 
@@ -94,6 +98,12 @@ class TrackSystem:
         for track_controller in track_controllers:
             # TODO(nns): Possibly add safety architecture here
             track_controller.set_block_occupancy(block_id, occupied)
+            track_controller.run_program()
+
+            new_authority = track_controller.get_authority_of_block(block_id)
+            if (new_authority is not None) and (occupied):
+                # Send the updated authority to this train
+                signals.trackmodel_update_authority.emit(train_id, new_authority)
 
         # Forward this information to the CTC
         signals.update_occupancy.emit(line, block_id, occupied)
