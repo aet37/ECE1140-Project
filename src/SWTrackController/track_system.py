b7b6c9bd370d1327fb3a9dfd006b39978108b95d
Luke Karavolis <karavolisl@gmail.com>
2020-11-28 19:18:02 -0500

    Half of Green Line (#257)
    * Add auto download
    
    * Half of green track
    
    * Add test

--- src/SWTrackController/track_system.py
+++ src/SWTrackController/track_system.py
@@ -88,14 +88,14 @@ class TrackSystem:
 
     def swtrack_update_occupancies(self, train_id, line, block_id, occupied):
         """Method connected to the swtrack_update_occupancies signal"""
-        logger.critical("Received swtrack_update_occupancies")
+        logger.debug("Received swtrack_update_occupancies")
 
         # Get the correct list of track controllers based on the line
         track_controllers = self.green_track_controllers if line == Line.LINE_GREEN else self.red_track_controllers
 
         # Set the occupancy of the specified block. This operation will only be
         # successful for the track controllers that operate the block
-        for track_controller in track_controllers:
+        for i, track_controller in enumerate(track_controllers):
             # TODO(nns): Possibly add safety architecture here
             track_controller.set_block_occupancy(block_id, occupied)
             track_controller.run_program()
@@ -104,6 +104,8 @@ class TrackSystem:
             if (new_authority is not None) and (occupied):
                 # Send the updated authority to this train
                 signals.trackmodel_update_authority.emit(train_id, new_authority)
+                logger.debug("New authority of {} found in track controller {} for train "
+                             "{} and block {}".format(new_authority, i, train_id, block_id))
 
         # Forward this information to the CTC
         signals.update_occupancy.emit(line, block_id, occupied)
@@ -121,8 +123,6 @@ class TrackSystem:
 
     def swtrack_set_track_heater(self, line, status):
         """Method connected to the swtrack_set_track_heater signal"""
-        logger.critical("Received swtrack_set_track_heater")
-
         # Get the correct list of track controllers based on the line
         track_controllers = self.green_track_controllers if line == Line.LINE_GREEN else self.red_track_controllers
 
